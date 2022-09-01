895244e6ac14e1a8eb8992ea0cc0229b34a9c4e5
zalan <mazalan01@gmail.com>
2022-09-01 15:28:48 +0100

    requires reboot command

--- pycheribuild/projects/syzkaller.py
+++ pycheribuild/projects/syzkaller.py
@@ -208,7 +208,6 @@ class VMSetting(Enum):
     MORELLO = {
             "type": "morello",
             "vm": {
-                "reboot_command": "timeout 1m /home/zalan/reboot_morello.sh",
                 "mbox_workdir": "/syz/"
                     }
                 }
@@ -381,10 +380,8 @@ class RunMorelloBaremetalSyzkaller(RunSyzkallerBase):
                                                               "with the morellobox")
         cls.syz_morellobox_address = cls.add_config_option("morellobox-address", show_help=True,
                                                            help="The address of the morellobox")
-        def_comm = VMSetting.MORELLO.value["vm"]["reboot_command"]
         cls.syz_reboot_command = cls.add_config_option("reboot-command", show_help=True,
-                                                       help="The command to restart the morellobox "
-                                                            "(default: '" + def_comm + "')")
+                                                       help="The command to restart the morellobox ")
         def_mbox_wdir = VMSetting.MORELLO.value["vm"]["mbox_workdir"]
         cls.syz_morellobox_workdir = cls.add_path_option("morellobox-workdir", show_help=True,
                                                          help="The directory where syzkaller fuzzing files are"
@@ -403,18 +400,21 @@ class RunMorelloBaremetalSyzkaller(RunSyzkallerBase):
             self.fatal("No morello box address provided, use --run-morello-baremetal-syzkaller/"
                        "morellobox-address ADDRESS")
             return
+        if self.syz_reboot_command is None:
+            self.fatal("No reboot command is provided, use --run-morello-baremetal-syzkaller/"
+                       "reboot-command COMMAND")
+            return
         kernel_path = kernel_project.get_kernel_install_path(kernel_config)
 
         template["kernel_obj"] = str(kernel_path)
         template["ssh_user"] = str(self.syz_ssh_user)
         template["sshkey"] = str(self.syz_ssh_key)
         template["vm"]["mbox_address"] = str(self.syz_morellobox_address)
+        template["vm"]["reboot_command"] = str(self.syz_reboot_command)
         if self.syz_morellobox_ssh_user is not None:
             template["vm"]["mbox_username"] = str(self.syz_morellobox_ssh_user)
         if self.syz_morellobox_ssh_key is not None:
             template["vm"]["mbox_sshkey"] = str(self.syz_morellobox_ssh_key)
-        if self.syz_reboot_command is not None:
-            template["vm"]["reboot_command"] = str(self.syz_reboot_command)
         if self.syz_morellobox_workdir is not None:
             template["vm"]["mbox_workdir"] = str(self.syz_morellobox_workdir)
 
